ce6b925dd487994c824f7e1819ca9a7b4593307b
Shoki Matsumoto <152942447+SK-Mat@users.noreply.github.com>
2023-12-05 13:36:55 +0900

    Add files via upload

--- GUI_jan.py
+++ GUI_jan.py
@@ -13,7 +13,8 @@ facility_dict = {}
 takatsuki = {"高槻2号":"68","高槻3号":"196","高槻シースー":"243"}
 umeda = {"お初天神":"94","北新地":"144","東通":"155","かっぱ横丁":"120","東通本店":"116","S茶屋町":"106","東通3号":"103","茶屋町":"100","東中通":"76","梅田芝田町":"199"}
 machine_list = ["JOYSOUND MAX GO","JOYSOUND MAX2","JOYSOUND MAX","LIVE DAM STADIUM","LIVE DAM Ai"]
-# now_status = "入力してください"
+now_status = "※複数回実行ボタンを押さないでください。"
+button_state = "able"
 
 def main():
     column = ("店舗名","JOYSOUND MAX GO","JOYSOUND MAX2","JOYSOUND MAX","LIVE DAM STADIUM","LIVE DAM Ai")
@@ -47,7 +48,7 @@ def main():
     use_number.set("人数")
     use_minute.set("利用時間(分)")
     coursebox.set("コース選択")
-    # status_label = ttk.Label(frame_1,text=now_status)
+    status_label = ttk.Label(frame_1,text=now_status)
         #---表---
     tree = ttk.Treeview(frame_3,columns = column)
     tree.heading("#0",text="")
@@ -70,9 +71,9 @@ def main():
     use_numberbox.pack(side=TOP)
     use_minutebox.pack(side=TOP)
     coursebox.pack(side=TOP)
-    button = ttk.Button(frame_2,text="実行",command=lambda:[create_facility_dict(var_umeda.get(),var_takatsuki.get()),mult_get_table([arrange_date(calender_date),hour.get()+":"+minute.get(),use_number.get(),use_minute.get(),course.get()],tree)])
+    button = ttk.Button(frame_2,text="実行",command=lambda:[create_facility_dict(var_umeda.get(),var_takatsuki.get()),mult_get_table([arrange_date(calender_date),hour.get()+":"+minute.get(),use_number.get(),use_minute.get(),course.get()],tree)],state=button_state)
     button.pack(side=TOP)
-    # status_label.pack(side=LEFT)
+    status_label.pack(side=LEFT)
     #---フレームの配置---
     frame_1.pack(side=TOP)
     frame_2.pack(side=TOP)
@@ -108,6 +109,7 @@ def btn_click(msg): #テスト用
 
 def get_urls(conditions_row): #URLを取得
     global facility_dict,urls
+    urls.clear() #初期化
     conditions = conditions_row
     #---URLに従うように表記を変更
     if conditions[4] == "通常":
@@ -123,7 +125,6 @@ def get_urls(conditions_row): #URLを取得
     elif len(conditions) == 5:
         for key,value in facility_dict.items():
             urls[key] = "https://jankara.me/reservation/custom/user/getReservationFormDisp?reservationType=1&facilityId="+value+"&targetDate="+conditions[0]+"&overDay=0&startHours="+conditions[1]+"&useNumber="+conditions[2]+"&useDate="+conditions[0]+"+"+conditions[1]+"&useTime="+conditions[3]+"&courseId="+conditions[4]+"&reReservationRoom=0&reReservationMachine=0&gaReservationType=0&gaCalendarViaType=1&searchId=65129198"
-    print(urls)
 def get_info(conditions_row):
     global machine_list
     get_urls(conditions_row)
@@ -135,7 +136,7 @@ def get_info(conditions_row):
         room_list["店名"] = key
         response = requests.get(url).text
         soup = BeautifulSoup(response, 'html.parser')
-        time.sleep(0.2)
+        time.sleep(1.0)
         for value,machine_name in [['input[value="10"]',"JOYSOUND MAX GO"],['input[value="7"]',"JOYSOUND MAX2"],['input[value="2"]',"JOYSOUND MAX"],['input[value="6"]',"LIVE DAM STADIUM"],['input[value="9"]',"LIVE DAM Ai"]]:
             for tag in soup.select(value):
                 left_room_num = tag.get("data-stock")
@@ -143,7 +144,6 @@ def get_info(conditions_row):
                     continue
                 room_list[machine_name] = left_room_num
         cnt=cnt+1
-        print(room_list)
         #--- room_listの整形---
         for machine_name in machine_list:
             if not machine_name in room_list:
@@ -151,22 +151,21 @@ def get_info(conditions_row):
             else:
                 pass
         tmp_room_list = {}
-        tmp_room_list
         for value in key_list:
             tmp_room_list[value] = room_list[value]
         room_list = tmp_room_list
-        print(room_list)
         copy_room_list = room_list.copy() #---room_listが毎回リセットされるのでコピーをもとにall_room_listを作成
         all_room_list.append(copy_room_list)
         room_list.clear()
-    print(all_room_list)
     return all_room_list
 
 def get_table(conditions_row,tree): #表に内容を記述
+    global button_state
     # global now_status
     # now_status = "処理中"
     # status_label.update()
     # print(now_status)
+    button_state = "disable"
     clear_table(tree) #古い表を削除
     #--- 不正な日時によるエラー処理 ---
     date_info = datetime.datetime(int(conditions_row[0].split("-")[0]),int(conditions_row[0].split("-")[1]),int(conditions_row[0].split("-")[2]),int(conditions_row[1].split(":")[0]),int(conditions_row[1].split(":")[1]),0)
